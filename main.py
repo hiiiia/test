51b5ff44fd8826fc019c7d0f0513277c68ac8a99
hiiia <gksqlc@gmail>
2022-10-12 17:21:29 +0900

    main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+print("경기 끔")
+
+score=[]
+
+for i in range(5):
+    j = int(input("평가점수 ==>"))
+    score.append(j)
