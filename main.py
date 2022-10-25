6e9ed1e187d0f2e3f11c22e508715ac26dcd15a7
hiiiia <90683622+hiiiia@users.noreply.github.com>
2022-10-25 23:26:29 +0900

    d
    d

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 list = []
 
 in_token = input("연산기호 입력 : ")
@@ -11,20 +11,3 @@ list.append(b)
 for i in list:
     print(i,end= ' ')
 
-
-
-=======
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-print("경기 끔")
-
-score=[]
-
-for i in range(5):
-    j = int(input("평가점수 ==>"))
-    score.append(j)
-print(score)
->>>>>>> a619e2f0c679053e9ae6cc3fdda1d2c37233b35b
