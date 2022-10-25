d00ed5ca35b9523a7b907674dcc23835ef3e8060
hiiia <gksqlc@gmail>
2022-10-25 22:35:58 +0900

    2

--- main.py
+++ main.py
@@ -7,8 +7,24 @@ b=input("두번째 입력: ")
 list.append(a)
 list.append(in_token)
 list.append(b)
-for i in list:
-    print(i,end= ' ')
+
+def calc(list):
+    a= list.pop()
+    b = list.pop()
+    c = list.pop()
+    ans = 0
+    if b == '*':
+        ans = int(c)*int(a)
+    elif b =='+':
+        ans = int(c) + int(a)
+    elif b =='-':
+        ans = int(c) - int(a)
+    elif b =='/':
+        ans = int(c) / int(a)
+
+    return ans , a ,b,c
+ans,a,b,c = calc(list)
+print(f"계산기 : {c} {b} {a} = {ans}")
 
 
 
